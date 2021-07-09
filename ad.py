3da6db2b091e74e573ed4ecd2e93a661a4b238c6
Bryan Useche <bryan.useche.10@gmail.com>
2021-07-08 22:58:56 -0400

    nueva funcion

--- ad.py
+++ ad.py
@@ -17,16 +17,22 @@ print("Host   : ", servername)
 print("IP     : ", addrIp)
 print("Dominio: ", domain)
 args = input("Es correcto?[S/n]: ")
+
+#Funcion que introduce los comandos y devuelve las respuestas
+def powershell(commands):
+    cmd = ''
+    cout = os.popen(cmd.join(commands)).read()
+    return cout
+
 if args == 'n' or args == 'N' or len(args) == 0:
     print("Corrija los datos y vuelva a correr el script")
 else:
     print("Se continuara la instalacion")
     #Empezamos a modificar los archivos que necesitamos
     #Modificamos el archivo /etc/hosts
-    command = ["echo '","%s    %s      %s.%s" % (addrIp, servername, servername, domain),"'>> /etc/hosts"]
-    cmd = ''
-    print(os.popen(cmd.join(command)).read())
-    
+    print(powershell(["echo '","%s    %s      %s.%s" % (addrIp, servername, servername, domain),"'>> /etc/hosts"]))
+    #Verificamos que el server este arriba
+    print(powershell(["ping –c 4", servername, ".", domain]))
 
     
         
